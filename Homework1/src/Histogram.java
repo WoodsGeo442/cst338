4bc396496a88cbf2166cbb8bd0179e28118b0258
WoodsGeo442 <gewoods@csumb.edu>
2020-02-07 02:44:56 -0800

    new code

--- Homework1/src/Histogram.java
+++ Homework1/src/Histogram.java
@@ -2,33 +2,31 @@ import java.util.Scanner;
 import java.io.*;
 
 public class Histogram {
-    private int[] count = new int[11];
-    private String[] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"};
-    public void tracker(String input){
-        if(input == letters[0]){
+    private static int[] count = new int[12];
+    private static String[] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"};
+    public static void tracker(String input){
+        if(input == "A"){
             count[0]++;
-        } else if(input == letters[1]){
+        } else if(input == "B"){
             count[1]++;
-        } else if(input == letters[2]){
+        } else if(input == "C"){
             count[2]++;
-        } else if(input == letters[3]){
+        } else if(input == "D"){
             count[3]++;
-        } else if(input == letters[4]){
+        } else if(input == "E"){
             count[4]++;
-        } else if(input == letters[5]){
+        } else if(input == "F"){
             count[5]++;
-        } else if(input == letters[6]){
+        } else if(input == "G"){
             count[6]++;
-        } else if(input == letters[7]){
+        } else if(input == "H"){
             count[7]++;
-        } else if(input == letters[8]){
+        } else if(input == "I"){
             count[8]++;
-        } else if(input == letters[9]){
+        } else if(input == "J"){
             count[9]++;
-        } else if(input == letters[10]){
+        } else if(input == "K"){
             count[10]++;
-        } else if(input == letters[11]){
-            count[11]++;
         } else {
             System.out.println("error. Try something else");
         }
@@ -54,9 +52,11 @@ public class Histogram {
 
             if(input.trim().toLowerCase().equals("zz")){
                 break;
+            } else {
+                System.out.println("You entered " + input);
+                tracker(input);
             }
-            System.out.println("You entered "+input);
-            tracker(input);
         }
     }
+
 }
