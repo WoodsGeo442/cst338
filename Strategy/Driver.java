d0f530fc60d892f46dedc38bcc6a1b2922d82c5d
WoodsGeo442 <gewoods@csumb.edu>
2020-03-06 17:28:50 -0800

    new

---
+++ Strategy/Driver.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Driver{
+    public static void main(String[] args){
+        HashMap<String, Integer> items = new HashMap<>();
+        items.put("gold", 5);
+        List<Monster> monsters = new ArrayList<>();
+
+    }
+}
